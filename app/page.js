a8c1de0d45293f354cb66470758fa52e709758e1
Prince34345 <priyanshudhawan55@gmail.com>
2025-06-04 20:59:08 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+      <div >
+           Hello Bro New to this Side
+           <br/>
+           <Button>Button</Button>
+      </div>
+  );
+}
