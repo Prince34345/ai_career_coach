50e9827b33857b605317cafa4eb1c3ff6087ba50
Prince34345 <priyanshudhawan55@gmail.com>
2025-06-07 17:01:30 +0530

    changes fixed

--- app/page.js
+++ app/page.js
@@ -3,9 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
       <div >
-           Hello Bro New to this Side
-           <br/>
-           <Button>Button</Button>
+        
       </div>
   );
 }
